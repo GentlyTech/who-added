eeffdbc6ef164cb9e8cc026af6a25b26d4ffddbf
GentlyTech <48165845+GentlyTech@users.noreply.github.com>
2023-01-28 00:54:23 -0500

    Stuff

--- src/app.tsx
+++ src/app.tsx
@@ -4,17 +4,20 @@ import Widget from "./widget";
 import manifest from "../package.json";
 import "./styles.css";
 
-function init(event: Event) {
-  let barElement = document.querySelector(".main-nowPlayingBar-left");
+async function main() {
+  while (!Spicetify?.showNotification) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+  
+  let barElement = document.querySelector(".main-nowPlayingWidget-nowPlaying");
 
-  if (!barElement) {
-    console.error(`Unable to load extension ${manifest.name} because the bar element could not be found.`);
-    Spicetify.showNotification("Unable to get parent container because the bar element could not be found", true);
-    return;
+  while (!barElement) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+    barElement = document.querySelector(".main-nowPlayingWidget-nowPlaying");
   }
 
   console.log(`Extension ${manifest.name} loaded!`);
-  Spicetify.showNotification("Parent container found!");
+  Spicetify.showNotification(`Extension ${manifest.name} loaded!`);
 
   const root = document.createElement("div");
   root.className += "WhoAddedWidgetContainer";
@@ -24,14 +27,6 @@ function init(event: Event) {
     <Widget />,
     root
   );
-}
-
-async function main() {
-  while (!Spicetify?.showNotification) {
-    await new Promise(resolve => setTimeout(resolve, 100));
-  }
-  
-  window.addEventListener("DOMContentLoaded", init);
 }
 
 export default main;
--- src/widget.tsx
+++ src/widget.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 export default function Widget() {
+  const [culprit, setCulprit] = useState(""); // The user that added the song
+  const [playlistSrc, setPlaylistSrc] = useState(""); // The user that added the song
+  const [avatarSrc, setAvatarSrc] = useState("");
+
   useEffect(() => {
     Spicetify.Player.addEventListener("songchange", (event) => {
-      if (!event || !event.data || !event.data.track || !event.data.track.metadata) {
-        Spicetify.showNotification("Song changed but unable to get song metadata");
-        return;
-      }
+      if (!event || !event.data || !event.data.track || !event.data.track.metadata) return;
+      const title = event.data.track.metadata["title"];
+      const contextURL = event.data.context_url;
+      if (playlistSrc !== contextURL) setPlaylistSrc(contextURL);
 
-      Spicetify.showNotification(`Now Playing ${event.data.track.metadata["title"]}`)
+      Spicetify.showNotification(`Now Playing ${title}`);
     });
 
     return () => {
@@ -18,7 +22,14 @@ export default function Widget() {
 
   return (
     <div className="WhoAddedWidgetContainer">
-      <h5>Hello world!</h5>
+      {culprit.length > 0 ? 
+      <div className="WhoAddedCulpritContainer">
+        <img src={avatarSrc} />
+        <h5>Added by {culprit}</h5>
+      </div> : null}
+      {playlistSrc.length > 0 ?
+        <h5>From {playlistSrc}</h5>
+      : null}
     </div>
   )
 }
