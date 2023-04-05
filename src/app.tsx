67362b84ffa059502a3a0a17d0b9622f70a2bc5e
GentlyTech <48165845+GentlyTech@users.noreply.github.com>
2023-04-04 20:12:17 -0400

    Add timeout to init

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,9 @@ import Widget from "./Widget";
 import manifest from "../package.json";
 import "./styles.css";
 
+const MAX_TRIES: number = 10;
+const EXTENSION_NAME: string = manifest.name;
+
 function App() {
   const [playerState, setPlayerState] = useState(Spicetify.Player.data);
 
@@ -26,18 +29,33 @@ function App() {
 }
 
 async function main() {
+  let tries: number = 0;
+
   while (!Spicetify?.showNotification || !Spicetify.CosmosAsync || !Spicetify.Player || !Spicetify.Platform || !Spicetify.URI) {
-    await new Promise(resolve => setTimeout(resolve, 100));
+    if (tries >= MAX_TRIES) {
+      console.error(`[${EXTENSION_NAME}] One (or more) of Spicetify's libraries did not load in a reasonable amount of time. Aborting...`);
+      return -1;
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    tries++;
   }
   
+  tries = 0;
   let barElement = document.querySelector(".main-nowPlayingBar-left");
 
   while (!barElement) {
-    await new Promise(resolve => setTimeout(resolve, 100));
+    if (tries >= MAX_TRIES) {
+      console.error(`[${EXTENSION_NAME}] Could not find the target element to inject into in a reasonable amount of time. Aborting...`);
+      return -1;
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
     barElement = document.querySelector(".main-nowPlayingBar-left");
+    tries++;
   }
 
-  console.log(`Extension ${manifest.name} loaded!`);
+  console.log(`[${EXTENSION_NAME}] Extension loaded!`);
   //Spicetify.showNotification(`Extension ${manifest.name} loaded!`);
 
   let root = document.createElement("div");
