b25b4a865539d75b89c2638f67b145dabaa62b70
GentlyTech <48165845+GentlyTech@users.noreply.github.com>
2025-04-15 03:53:51 -0400

    Add Info icon

---
+++ assets/InfoIcon.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const InfoIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="lucide lucide-info-icon lucide-info"
+  >
+    <circle cx="12" cy="12" r="10" />
+    <path d="M12 16v-4" />
+    <path d="M12 8h.01" />
+  </svg>
+);
